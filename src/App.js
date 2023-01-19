a9cc3bd9ca1d04fae11bbc1a3b3c646c8995f1a9
Lani <ooluwalani@gmail.com>
2023-01-19 00:13:24 -0500

    setup project and UI design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './calculator.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const calc = () => {
+  return ( 
+    <div className="calculator-container">
+      <div className='output'>
+        <div className='prev_operand'>124,895 *</div>
+        <div className='curr_operand'>124,895</div>
+      </div>
+      {/* <div className='buttons'> */}
+        <button>+</button>
+        <button>-</button>
+        <button>x</button>
+        <button>/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className='equal_sign'>=</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>0</button>
+        <button>.</button>
+        <button>AC</button>
+      {/* </div> */}
     </div>
-  );
+   );
 }
-
-export default App;
+ 
+export default calc;
